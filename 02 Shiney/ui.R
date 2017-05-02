e6c6e220f43e46ed07db90a02d0edaebd592359f
Jacobbtemple <jacobbtemple@gmail.com>
2017-05-01 19:47:04 -0500

    Update ui.R

--- 02 Shiney/ui.R
+++ 02 Shiney/ui.R
@@ -1,45 +1,86 @@
 #ui.R
 require(shiny)
 require(shinydashboard)
+require(DT)
+require(leaflet)
+require(plotly)
 
 dashboardPage(
   dashboardHeader(
   ),
   dashboardSidebar(
     sidebarMenu(
+      menuItem("Box Plots", tabName = "boxplot", icon = icon("dashboard")),
+      menuItem("Histograms", tabName = "histogram", icon = icon("dashboard")),
+      menuItem("Scatter Plots", tabName = "scatter", icon = icon("dashboard")),
+      menuItem("Crosstabs, KPIs, Parameters", tabName = "crosstab", icon = icon("dashboard")),
       menuItem("Barcharts, Table Calculations", tabName = "barchart", icon = icon("dashboard"))
     )
   ),
-  dashboardBody(    
-    tabItems(tabItem(tabName = "scatter",
+  dashboardBody(
+    tags$style(type="text/css",
+               ".shiny-output-error { visibility: hidden; }",
+               ".shiny-output-error:before { visibility: hidden; }"
+    ),    
+    tabItems(
+      # Begin Box Plots tab content.
+      tabItem(tabName = "boxplot",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb5", "Get data from:",
+                                      c("SQL" = "SQL"), inline=T),
+                         uiOutput("boxplotRegions"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html,
+                         actionButton(inputId = "click5",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("boxplotData1")
+                ),
+                tabPanel("Simple Box Plot", 
+                         sliderInput("boxSalesRange1", "Sales Range:", # See https://shiny.rstudio.com/articles/sliders.html
+                                     min = min(globals$Sales), max = max(globals$Sales), 
+                                     value = c(min(globals$Sales), max(globals$Sales))),
+                         sliderInput("range5a", "Loop through Quarters:", 
+                                     min(globals$Order_Date), 
+                                     max(globals$Order_Date) + .75, 
+                                     max(globals$Order_Date), 
+                                     step = 0.25,
+                                     animate=animationOptions(interval=2000, loop=T)),
+                         plotlyOutput("boxplotPlot1", height=500))
+              )
+      ),
+      # End Box Plots tab content.
+      # Begin Histogram tab content.
+      tabItem(tabName = "histogram",
+              tabsetPanel(
+                tabPanel("Data",  
+                         radioButtons("rb4", "Get data from:",
+                                      c("SQL" = "SQL",
+                                        "CSV" = "CSV"), inline=T),
+                         actionButton(inputId = "click4",  label = "To get data, click here"),
+                         hr(), # Add space after button.
+                         DT::dataTableOutput("histogramData1")
+                ),
+                tabPanel("Simple Histogram", plotlyOutput("histogramPlot1", height=1000))
+              )
+      ),
+      # End Histograms tab content.
+      # Begin Scatter Plots tab content.
+      tabItem(tabName = "scatter",
               tabsetPanel(
                 tabPanel("Data",  
                          radioButtons("rb3", "Get data from:",
-                                      c("SQL" = "SQL",
-                                        "CSV" = "CSV"), inline=T),
+                                      c("SQL" = "SQL"), inline=T),
                          uiOutput("scatterStates"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html,
                          actionButton(inputId = "click3",  label = "To get data, click here"),
                          hr(), # Add space after button.
                          DT::dataTableOutput("scatterData1")
                 ),
-                tabPanel("Political Change over Turbines built: 2008 - 2014", plotlyOutput("scatterPlot1", height=1000),plotlyOutput("scatterPlot2", height=2000))
+                tabPanel("Political Change over Turbines built: 2008 - 2014", plotlyOutput("scatterPlot1", height=400),plotlyOutput("scatterPlot2", height=400),plotlyOutput("scatterPlot3", height=400))
                 
-              ),
-      
-      # Begin Barchart tab content.
-      tabItem(tabName = "barchart",
-        tabsetPanel(
-          tabPanel("Data",
-                   uiOutput("regions2"), # See http://shiny.rstudio.com/gallery/dynamic-ui.html
-                   actionButton(inputId = "click2",  label = "To get data, click here"),
-                   hr(), # Add space after button.
-                   DT::dataTableOutput("data2")
-          ),
-          tabPanel("Barchart", "Black = Sum of Sales per Region, Red = Average Sum of Sales per Category, and  Blue = (Sum of Sales per Region - Average Sum of Sales per Category)", plotOutput("plot2", height=1500))
-        )
+              )
       )
-      # End Barchart tab content.
+      # End Scatter Plots tab content.
+      # Begin Crosstab tab content.
+      
     )
   )
 )
-
